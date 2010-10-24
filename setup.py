b950335bcd4b78928b02fcb5ae607d488366fc18
Wilbert Berendsen <wbsoft@xs4all.nl>
2010-10-24 12:05:07 +0000

    suppport poppler versions from 0.12.0 on by disabling sip definitions based on the poppler version found; version is determined using pkg-config
    git-svn-id: https://python-poppler-qt4.googlecode.com/svn/trunk/python-poppler-qt4@13 83130c62-50b3-82d2-30ac-6e1d6613df04

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ project = dict(
 )
 
 import os
+import re
 import shlex
 import subprocess
 
@@ -38,7 +39,7 @@ pyqt_sip_flags = config.pyqt_sip_flags
 qt_inc_dir = config.qt_inc_dir
 
 
-def pkg_config(package, attrs=None):
+def pkg_config(package, attrs=None, include_only=False):
     """parse the output of pkg-config for a package.
     
     returns the given or a new dictionary with one or more of these keys
@@ -48,10 +49,14 @@ def pkg_config(package, attrs=None):
     """
     if attrs is None:
         attrs = {}
+    cmd = ['pkg-config']
+    if include_only:
+        cmd += ['--cflags-only-I']
+    else:
+        cmd += ['--cflags', '--libs']
+    cmd.append(package)
     try:
-        output = subprocess.Popen(
-            ['pkg-config', '--cflags', '--libs', package],
-            stdout=subprocess.PIPE).communicate()[0]
+        output = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
     except OSError:
         return attrs
     flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
@@ -63,23 +68,15 @@ def pkg_config(package, attrs=None):
                 l.append(path)
     return attrs
 
-
-# hack to provide our options to sip on its invocation:
-class build_ext(sipdistutils.build_ext):
-    def _sip_compile(self, sip_bin, source, sbf):
-        cmd = [sip_bin]
-        if hasattr(self, 'sip_opts'):
-            cmd += self.sip_opts
-        if hasattr(self, '_sip_sipfiles_dir'):
-            cmd += ['-I', self._sip_sipfiles_dir()]
-        cmd += [
-            "-c", self.build_temp,
-            "-b", sbf,
-            "-I", pyqt_sip_dir]             # find the PyQt4 stuff
-        cmd += shlex.split(pyqt_sip_flags)  # use same SIP flags as for PyQt4
-        cmd.append(source)
-        self.spawn(cmd)
-
+def pkg_config_version(package):
+    """Returns the version of the given package as a tuple of ints."""
+    cmd = ['pkg-config', '--modversion', package]
+    try:
+        output = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
+        return tuple(map(int, re.findall(r'\d+', output)))
+    except OSError:
+        sys.stderr.write("Can't determine version of %s\n" % package)
+        
 
 ext_args = {
     'include_dirs': [
@@ -96,9 +93,40 @@ pkg_config('poppler-qt4', ext_args)
 if 'libraries' not in ext_args:
     ext_args['libraries'] = ['poppler-qt4']
 
+
+# hack to provide our options to sip on its invocation:
+class build_ext(sipdistutils.build_ext):
+    def _sip_compile(self, sip_bin, source, sbf):
+        
+        # Disable features if older poppler-qt4 version is found.
+        # Default to most recent version supported by our binding.
+        # See the defined tags in %Timeline{} in poppler-qt4.sip.
+        ver = pkg_config_version('poppler-qt4')
+        if not ver or ver >= (0, 13, 2):
+            tag = 'POPPLER_V0_13_2'
+        elif ver >= (0, 12, 1):
+            tag = 'POPPLER_V0_12_1'
+        else:
+            tag = 'POPPLER_V0_12_0'
+        
+        cmd = [sip_bin]
+        if hasattr(self, 'sip_opts'):
+            cmd += self.sip_opts
+        if hasattr(self, '_sip_sipfiles_dir'):
+            cmd += ['-I', self._sip_sipfiles_dir()]
+        if tag:
+            cmd += ['-t', tag]
+        cmd += [
+            "-c", self.build_temp,
+            "-b", sbf,
+            "-I", pyqt_sip_dir]             # find the PyQt4 stuff
+        cmd += shlex.split(pyqt_sip_flags)  # use same SIP flags as for PyQt4
+        cmd.append(source)
+        self.spawn(cmd)
+
+
 setup(
     cmdclass = {'build_ext': build_ext},
     ext_modules = [Extension("popplerqt4", ["poppler-qt4.sip"], **ext_args)],
     **project
 )
-
