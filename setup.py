6495ec7441eb1bc6e068345ebc68d9d6f96e6291
Wilbert Berendsen <wbsoft@xs4all.nl>
2010-10-24 00:05:47 +0000

    use pkg-config if available to find poppler-qt4; make setup script work with 4.9.1 versions of sip
    git-svn-id: https://python-poppler-qt4.googlecode.com/svn/trunk/python-poppler-qt4@11 83130c62-50b3-82d2-30ac-6e1d6613df04

--- setup.py
+++ setup.py
@@ -1,34 +1,6 @@
 #! python
 
-import os
-
-from distutils.core import setup, Extension
-import sipdistutils
-
-import PyQt4.pyqtconfig as pyqtconfig
-config = pyqtconfig.Configuration()
-
-pyqt_sip_dir = config.pyqt_sip_dir
-pyqt_sip_flags = config.pyqt_sip_flags
-qt_inc_dir = config.qt_inc_dir
-
-# TODO: use e.g. pkg-config
-poppler_inc_dir = "/usr/include/poppler"
-
-# hack to provide our options to sip on its invocation:
-class build_ext(sipdistutils.build_ext):
-    def _sip_compile(self, sip_bin, source, sbf):
-        self.spawn(
-            [sip_bin] + self.sip_opts +
-            ["-c", self.build_temp,
-            "-b", sbf,
-            "-I", self._sip_sipfiles_dir(),
-            "-I", pyqt_sip_dir,             # find the PyQt4 stuff
-            ] + pyqt_sip_flags.split() + [  # use same SIP flags as for PyQt4
-            source])
-
-
-setup(
+project = dict(
     name = 'python-poppler-qt4',
     version = '0.1.1',
     description = 'A Python binding to Poppler-Qt4',
@@ -49,19 +21,84 @@ setup(
         'Programming Language :: Python',
         'Topic :: Multimedia :: Graphics :: Viewers',
     ],
-    ext_modules =  [
-        Extension(
-            "popplerqt4", ["poppler-qt4.sip"],
-            include_dirs = [
-                qt_inc_dir,
-                os.path.join(qt_inc_dir, 'QtCore'),
-                os.path.join(qt_inc_dir, 'QtGui'),
-                os.path.join(qt_inc_dir, 'QtXml'),
-                poppler_inc_dir,
-            ],
-            libraries = ["poppler-qt4"],
-        ),
+)
+
+import os
+import shlex
+import subprocess
+
+from distutils.core import setup, Extension
+import sipdistutils
+
+import PyQt4.pyqtconfig as pyqtconfig
+config = pyqtconfig.Configuration()
+
+pyqt_sip_dir = config.pyqt_sip_dir
+pyqt_sip_flags = config.pyqt_sip_flags
+qt_inc_dir = config.qt_inc_dir
+
+
+def pkg_config(package, attrs=None):
+    """parse the output of pkg-config for a package.
+    
+    returns the given or a new dictionary with one or more of these keys
+    'include_dirs', 'library_dirs', 'libraries'. Every key is a list of paths,
+    so that it can be used with distutils Extension objects.
+    
+    """
+    if attrs is None:
+        attrs = {}
+    try:
+        output = subprocess.Popen(
+            ['pkg-config', '--cflags', '--libs', package],
+            stdout=subprocess.PIPE).communicate()[0]
+    except OSError:
+        return attrs
+    flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
+    for flag in shlex.split(output):
+        option, path = flag[:2], flag[2:]
+        if option in flag_map:
+            l = attrs.setdefault(flag_map[option], [])
+            if path not in l:
+                l.append(path)
+    return attrs
+
+
+# hack to provide our options to sip on its invocation:
+class build_ext(sipdistutils.build_ext):
+    def _sip_compile(self, sip_bin, source, sbf):
+        cmd = [sip_bin]
+        if hasattr(self, 'sip_opts'):
+            cmd += self.sip_opts
+        if hasattr(self, '_sip_sipfiles_dir'):
+            cmd += ['-I', self._sip_sipfiles_dir()]
+        cmd += [
+            "-c", self.build_temp,
+            "-b", sbf,
+            "-I", pyqt_sip_dir]             # find the PyQt4 stuff
+        cmd += shlex.split(pyqt_sip_flags)  # use same SIP flags as for PyQt4
+        cmd.append(source)
+        self.spawn(cmd)
+
+
+ext_args = {
+    'include_dirs': [
+        qt_inc_dir,
+        os.path.join(qt_inc_dir, 'QtCore'),
+        os.path.join(qt_inc_dir, 'QtGui'),
+        os.path.join(qt_inc_dir, 'QtXml'),
+        '/usr/include/poppler',
     ],
-    cmdclass = {'build_ext': build_ext}
+}
+
+pkg_config('poppler-qt4', ext_args)
+
+if 'libraries' not in ext_args:
+    ext_args['libraries'] = ['poppler-qt4']
+
+setup(
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [Extension("popplerqt4", ["poppler-qt4.sip"], **ext_args)],
+    **project
 )
 
